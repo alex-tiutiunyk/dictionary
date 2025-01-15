d657a884be56e637500281622678a406fbc84d41
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2025-01-15 11:14:40 +0200

    Add Sign Out functional in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,17 @@
+import { getAuth, signOut } from 'firebase/auth';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+  const auth = getAuth();
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      console.log('Sign Out');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <header className='fixed w-full bg-amber-200'>
       <div className='max-w-[920px] mx-auto pl-5 pr-5 pt-2 pb-2'>
@@ -24,6 +35,13 @@ const Header = () => {
             </NavLink>
           </li>
         </ul>
+        <button
+          type='button'
+          className='absolute top-2 right-2 hover:underline'
+          onClick={handleSignOut}
+        >
+          Sign Out
+        </button>
       </div>
     </header>
   );
