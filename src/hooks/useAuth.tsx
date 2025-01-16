01a907e52ac11dde7693f202b5b36c5248f97977
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2025-01-16 10:50:30 +0200

    Create a custom hook to manage the authentication

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+
+const useAuth = () => {
+  const [user, setUser] = React.useState<User | null>(null);
+  const [loading, setLoading] = React.useState<boolean>(true);
+
+  const auth = getAuth();
+
+  React.useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) setUser(user);
+      else setUser(null);
+      setLoading(false);
+      console.log(user);
+    });
+    return () => {
+      listen();
+    };
+  }, []);
+  return { user, loading };
+};
+
+export default useAuth;
