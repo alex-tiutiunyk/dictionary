da5a3b726dab1e6c0538465558c7dea603c8afe2
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2025-01-19 13:38:50 +0200

    Add open close block for additional info

--- src/components/WordItem.tsx
+++ src/components/WordItem.tsx
@@ -11,6 +11,8 @@ interface WordItemProps {
 }
 
 const WordItem = ({ word }: WordItemProps) => {
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+
   const user = useAppSelector((state) => state.user.value) as User;
   const userId = user.email as string;
   const dispatch = useAppDispatch();
@@ -22,23 +24,56 @@ const WordItem = ({ word }: WordItemProps) => {
     getWordsFunc(user, 'users', 'words').then((data) => dispatch(getAllWords(data)));
   };
 
+  const handleOpen = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <li key={word.id} className='flex justify-between  px-2 py-1 bg-gray-100'>
-      <div>
-        {word.word} - <span className='text-indigo-500'>{word.wordTranslation}</span>
+    <li key={word.id}>
+      <div className='flex justify-between px-2 py-1 bg-gray-100'>
+        <div>
+          {word.word} - <span className='text-indigo-500'>{word.wordTranslation}</span>
+        </div>
+        <div className='flex gap-2'>
+          <button
+            type='button'
+            className={`border border-gray-700 px-0.5 text-red-600 ${
+              !word.example || !word.exampleTranslation || !word.comment
+                ? 'opacity-40 cursor-default'
+                : ''
+            }`}
+            onClick={handleOpen}
+          >
+            <CornerRightDown />
+          </button>
+          <button
+            type='button'
+            className='border border-gray-700 px-0.5'
+            onClick={() => deleteWord(word.id, userId)}
+          >
+            delete
+          </button>
+        </div>
       </div>
-      <div className='flex gap-2'>
-        <button type='button' className='border border-gray-700 px-0.5 text-red-600'>
-          <CornerRightDown />
-        </button>
-        <button
-          type='button'
-          className='border border-gray-700 px-0.5'
-          onClick={() => deleteWord(word.id, userId)}
-        >
-          delete
-        </button>
-      </div>
+      {isOpen && (word.example || word.exampleTranslation || word.comment) && (
+        <div className='bg-gray-50 px-2 py-1'>
+          {word.example && (
+            <p>
+              <b>Example:</b> {word.example}
+            </p>
+          )}
+          {word.exampleTranslation && (
+            <p>
+              <b>Translate:</b> {word.exampleTranslation}
+            </p>
+          )}
+          {word.comment && (
+            <p>
+              <b>Comment:</b> {word.comment}
+            </p>
+          )}
+        </div>
+      )}
     </li>
   );
 };
