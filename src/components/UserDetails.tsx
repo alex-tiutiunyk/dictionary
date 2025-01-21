9455f2785da836d37e6417110d278de8787581ad
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2025-01-21 21:11:15 +0200

    Add user drop down

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -5,6 +5,7 @@ import { auth } from '../firebase';
 import { useAppSelector } from '../redux/hooks';
 
 const UserDetails: React.FC = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
   const user = useAppSelector((state) => state.user.value);
   const navigate = useNavigate();
 
@@ -13,16 +14,39 @@ const UserDetails: React.FC = () => {
       await signOut(auth);
       navigate('/dictionary/signin');
       console.log('Sign Out');
+      setIsOpen((prev) => !prev);
     } catch (error) {
       console.log(error);
     }
   };
 
+  const handleUser = () => {
+    navigate('/dictionary/user');
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleDropDown = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
     <div className='absolute top-2 right-[20px]'>
-      <button type='button' onClick={handleSignOut}>
-        {`${user?.displayName ? user?.displayName : user?.email}`}
-      </button>
+      <span onClick={handleDropDown} className='cursor-pointer'>{`${
+        user?.displayName ? user?.displayName : user?.email
+      }`}</span>
+      {isOpen && (
+        <div className='absolute top-full right-[-5px] border bg-white shadow-md py-2 min-w-[120px] rounded-lg'>
+          <ul>
+            <li></li>
+            <li onClick={handleUser} className='cursor-pointer hover:bg-slate-100 px-2'>
+              Settings
+            </li>
+            <li onClick={handleSignOut} className='cursor-pointer hover:bg-slate-100 px-2'>
+              Log Out
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
